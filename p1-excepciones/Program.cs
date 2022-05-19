d8ddbba7cd4889d7656523377217cd79fa2128bd
Olaf Huerta <l21211966@tectijuana.edu.mx>
2022-05-19 11:50:19 -0700

    p1 - excepciones
    Programa de excepciones

---
+++ p1-excepciones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1_excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadKey();
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.Write(e.Message);
+                Console.ReadKey();
+            }
+            catch (FormatException e)
+            {
+                Console.Write(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
